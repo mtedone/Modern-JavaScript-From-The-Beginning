733510bf27f70591285f337d51ef568247713782
Marco Tedone <marco.tedone@gmail.com>
2019-12-11 16:43:00 +0000

    Added code for collection selectors

--- V11411_Code File/S03/3_4_project_files/js_sandbox/app.js
+++ V11411_Code File/S03/3_4_project_files/js_sandbox/app.js
@@ -1,51 +1,48 @@
-// document.getElementsByClassName
+// get elements by class name
 
 // const items = document.getElementsByClassName('collection-item');
 // console.log(items);
+//
 // console.log(items[0]);
 // items[0].style.color = 'red';
-// items[3].textContent = 'Hello';
-
+//
 // const listItems = document.querySelector('ul').getElementsByClassName('collection-item');
-
 // console.log(listItems);
 
-// document.getElementsByTagName
+// get elements by tag name
 // let lis = document.getElementsByTagName('li');
 // console.log(lis);
+//
 // console.log(lis[0]);
 // lis[0].style.color = 'red';
-// lis[3].textContent = 'Hello';
-
-// // Conver HTML Collection into array
+//
 // lis = Array.from(lis);
-
 // lis.reverse();
-
-// lis.forEach(function(li, index){
-//   console.log(li.className);
-//   li.textContent = `${index}: Hello`;
+// lis.forEach(function(li, index) {
+//     console.log(li.className);
+//     li.textContent = `${index}: Hello`;
 // });
+// console.log(lis)
 
-// console.log(lis);
+// Query selector all
 
-// document.querySelectorAll
 const items = document.querySelectorAll('ul.collection li.collection-item');
 
-items.forEach(function(item, index){
-    item.textContent = `${index}: Hello`;
+items.forEach(function (li, index) {
+    li.textContent = `${index}: Hello`;
+
 });
 
 const liOdd = document.querySelectorAll('li:nth-child(odd)');
 const liEven = document.querySelectorAll('li:nth-child(even)');
 
-liOdd.forEach(function(li, index){
-  li.style.background = '#ccc';
+items.forEach(function (li, index) {
+    li.style.background = '#ccc';
+
 });
 
-for(let i = 0; i < liEven.length; i++){
-  liEven[i].style.background = '#f4f4f4';
+for(let i = 0; i < liEven.length; i++) {
+    liEven[i].style.background = '#f4f4f4';
 }
-
 
 console.log(items);
