1d117edea070180f8cb6e04a129ae0fad93268fb
Marco Tedone <marco.tedone@gmail.com>
2019-12-11 16:08:06 +0000

    Added some js statements to inspect document

--- V11411_Code File/S03/3_2_project_files/js_sandbox/js_sandbox/app.js
+++ V11411_Code File/S03/3_2_project_files/js_sandbox/js_sandbox/app.js
@@ -2,7 +2,7 @@ let val;
 
 val = document;
 val = document.all;
-val = document.all[2];
+val = document.all[1];
 val = document.all.length;
 val = document.head;
 val = document.body;
@@ -12,17 +12,14 @@ val = document.URL;
 val = document.characterSet;
 val = document.contentType;
 
-val = document.forms;
 val = document.forms[0];
 val = document.forms[0].id;
 val = document.forms[0].method;
-val = document.forms[0].action;
 
 val = document.links;
 val = document.links[0];
 val = document.links[0].id;
 val = document.links[0].className;
-val = document.links[0].classList[0];
 
 val = document.images;
 
@@ -30,11 +27,12 @@ val = document.scripts;
 val = document.scripts[2].getAttribute('src');
 
 let scripts = document.scripts;
-
-let scriptsArr = Array.from(scripts);
-
-scriptsArr.forEach(function(script) {
+let scriptsArray = Array.from(scripts);
+scriptsArray.forEach(function (script) {
   console.log(script.getAttribute('src'));
 });
+
+
+
 
 console.log(val);
