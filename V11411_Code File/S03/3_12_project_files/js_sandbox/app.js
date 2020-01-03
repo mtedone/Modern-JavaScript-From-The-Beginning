02f64100067dba134910a7aa8975eb9a6baed161
Marco Tedone <marco.tedone@gmail.com>
2020-01-03 13:58:49 +0000

    Worked with local storage

--- V11411_Code File/S03/3_12_project_files/js_sandbox/app.js
+++ V11411_Code File/S03/3_12_project_files/js_sandbox/app.js
@@ -1,44 +1,42 @@
-// set local storage item
+// // Set local storage item
 // localStorage.setItem('name', 'John');
 // localStorage.setItem('age', '30');
-
-// set session storage item
+//
+// // Set session storate
 // sessionStorage.setItem('name', 'Beth');
 
-// remove from storage
+// Remove from storage
 // localStorage.removeItem('name');
 
-// get from storage
+// get value from storage
 // const name = localStorage.getItem('name');
 // const age = localStorage.getItem('age');
 
-// // clear local storage
+// Clear local storage
 // localStorage.clear();
-
 // console.log(name, age);
 
-document.querySelector('form').addEventListener('submit', function(e){
-  const task = document.getElementById('task').value;
+document.querySelector('form').addEventListener('submit',
+    function(e) {
 
-  let tasks;
+      const task = document.getElementById('task').value;
 
-  if(localStorage.getItem('tasks') === null) {
-    tasks = [];
-  } else {
-    tasks = JSON.parse(localStorage.getItem('tasks'));
-  }
+      let tasks;
 
-  tasks.push(task);
+      if (localStorage.getItem('tasks' ) === null) {
+        tasks = [];
+      } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+      }
 
-  localStorage.setItem('tasks', JSON.stringify(tasks));
-
-  alert('Task saved');
-
-  e.preventDefault();
-});
+      tasks.push(task);
+      localStorage.setItem('tasks', JSON.stringify(tasks));
+      console.log(task)
+      e.preventDefault();
+    });
 
 const tasks = JSON.parse(localStorage.getItem('tasks'));
 
-tasks.forEach(function(task){
-  console.log(task);
+tasks.forEach(function(task) {
+  console.log(task)
 });
