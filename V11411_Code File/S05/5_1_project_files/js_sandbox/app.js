9f7313817f0049d210e9d423c94b12631600d01a
Marco Tedone <marco.tedone@gmail.com>
2020-05-08 20:02:33 +0100

    Created constructor

--- V11411_Code File/S05/5_1_project_files/js_sandbox/app.js
+++ V11411_Code File/S05/5_1_project_files/js_sandbox/app.js
@@ -3,17 +3,14 @@ function Person(name, dob) {
   this.name = name;
   // this.age = age;
   this.birthday = new Date(dob);
-  this.calculateAge = function(){
-    const diff =  Date.now() - this.birthday.getTime();
+  this.calculateAge = function() {
+    const diff = Date.now() - this.birthday.getTime();
     const ageDate = new Date(diff);
     return Math.abs(ageDate.getUTCFullYear() - 1970);
   }
 }
 
 // const brad = new Person('Brad', 36);
-// const john = new Person('John', 30);
-
-// console.log(john.age);
-
-const brad = new Person('Brad', '9-10-1981');
+// const brad = new Person('John', 30);
+const brad = new Person('Brad', '1972-05-20');
 console.log(brad.calculateAge());
