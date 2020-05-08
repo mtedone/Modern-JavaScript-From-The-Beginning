3809a48ddd8d8a0f729faa61248b3608e1396479
Marco Tedone <marco.tedone@gmail.com>
2020-05-08 12:29:53 +0100

    Created basic UI with winning logic

---
+++ V11411_Code File/S04/4_7_project_files/numberguesser/app.js
@@ -0,0 +1,53 @@
+/*
+ GAME FUNCTION:
+ - Player must guess a number between a min and a max
+ - Player gets a certain amount of guesses
+ - Notify player of guesses remaining
+ - Notify the player of the correct answer if loose
+ - Let player choose to play again
+ */
+
+// Game values
+let min = 1,
+    max = 10,
+    winningNum = 2,
+    guessesLeft  = 3;
+
+// UI elements
+const game = document.getElementById('game'),
+      minNum = document.querySelector('.min-num'),
+      maxNum = document.querySelector('.max-num'),
+      guessBtn = document.getElementById('guess-btn'),
+      guessInput = document.getElementById('guess-input'),
+      message = document.querySelector('.message');
+
+// Assign UI min and max
+minNum.textContent = min;
+maxNum.textContent = max;
+
+// Listens for guess
+guessBtn.addEventListener('click', function () {
+    let guess = parseInt(guessInput.value);
+
+    //Validate
+    if (isNaN(guess) || guess < min || guess > max) {
+        setMessage(`Please enter a number between ${min} and ${max}`, 'red');
+    }
+
+    // Check if won
+    if (guess === winningNum) {
+        // Disable input
+        guessInput.disabled = true;
+        // Change border to green to show they've wom
+        guessInput.style.borderColor = 'green';
+        // Set message
+        setMessage(`${winningNum} is correct! YOU WIN!`, 'green');
+    } else {
+
+    }
+});
+
+function setMessage(msg, color) {
+    message.style.color = color;
+    message.textContent = msg;
+}
