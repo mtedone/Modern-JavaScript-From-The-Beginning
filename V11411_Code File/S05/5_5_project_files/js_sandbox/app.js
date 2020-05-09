c9c4473611ef2603c099853437b7fafd3a18f40f
Marco Tedone <marco.tedone@gmail.com>
2020-05-09 10:49:24 +0100

    Studied prototypes

--- V11411_Code File/S05/5_5_project_files/js_sandbox/app.js
+++ V11411_Code File/S05/5_5_project_files/js_sandbox/app.js
@@ -1,27 +1,26 @@
 const personPrototypes = {
-  greeting: function() {
+  greeting: function () {
     return `Hello there ${this.firstName} ${this.lastName}`;
   },
-  getsMarried: function(newLastName) {
+  getsMarried: function (newLastName) {
     this.lastName = newLastName;
   }
 }
 
 const mary = Object.create(personPrototypes);
 mary.firstName = 'Mary';
-mary.lastName = 'Williams';
+mary.lastName = 'Poppins';
 mary.age = 30;
 
 mary.getsMarried('Thompson');
 
 console.log(mary.greeting());
 
-const brad = Object.create(personPrototypes, {
-  firstName: {value: 'Brad'},
-  lastName: {value: 'Traversy'},
-  age: {value: 36}
+const marco = Object.create(personPrototypes, {
+  firstName: {value: 'Marco'},
+  lastName: {value: 'Tedone'},
+  age: {value: 47}
 });
 
-console.log(brad);
+console.log(marco);
 
-console.log(brad.greeting());
