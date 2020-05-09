9db93581ff8051dd983e5730237fe3861b78b5e6
Marco Tedone <marco.tedone@gmail.com>
2020-05-09 12:46:39 +0100

    Added inline memory behaviour

--- V11411_Code File/S06/6_2_project_files/booklist/app.js
+++ V11411_Code File/S06/6_2_project_files/booklist/app.js
@@ -1,54 +1,52 @@
-// Book Constructor
+// Book constructor
 function Book(title, author, isbn) {
   this.title = title;
   this.author = author;
   this.isbn = isbn;
 }
 
-// UI Constructor
+// UI constructor
 function UI() {}
 
-// Add Book To List
-UI.prototype.addBookToList = function(book){
+UI.prototype.addBookToList = function(book) {
   const list = document.getElementById('book-list');
-  // Create tr element
+  // Create a TR element
   const row = document.createElement('tr');
-  // Insert cols
+  // Insers cols
   row.innerHTML = `
     <td>${book.title}</td>
     <td>${book.author}</td>
     <td>${book.isbn}</td>
-    <td><a href="#" class="delete">X<a></td>
+    <td><a href="#" class="delete">X</a> </td>
   `;
-
   list.appendChild(row);
 }
 
-// Clear Fields
+// Clear fields
 UI.prototype.clearFields = function() {
   document.getElementById('title').value = '';
   document.getElementById('author').value = '';
   document.getElementById('isbn').value = '';
 }
 
-// Event Listeners
-document.getElementById('book-form').addEventListener('submit', function(e){
-  // Get form values
-  const title = document.getElementById('title').value,
-        author = document.getElementById('author').value,
-        isbn = document.getElementById('isbn').value
+// Event listeners
+document.getElementById('book-form').addEventListener('submit',
+    function (e) {
+      // Get form values
+      const title = document.getElementById('title').value,
+            author = document.getElementById('author').value,
+            isbn = document.getElementById('isbn').value;
 
-  // Instantiate book
-  const book = new Book(title, author, isbn);
+      const book = new Book(title, author, isbn);
 
-  // Instantiate UI
-  const ui = new UI();
- 
-  // Add book to list
-  ui.addBookToList(book);
+      // Instantiates the UI object
+      const ui = new UI();
 
-  // Clear fields
-  ui.clearFields();
+      // Add book to list
+      ui.addBookToList(book);
 
-  e.preventDefault();
-});
+      // Clears fields
+      ui.clearFields();
+
+      e.preventDefault();
+    });
