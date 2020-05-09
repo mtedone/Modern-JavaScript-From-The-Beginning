d232401d5b947acf1cb357cae324476b0fc62d14
Marco Tedone <marco.tedone@gmail.com>
2020-05-09 10:40:23 +0100

    Studied prototypes

--- V11411_Code File/S05/5_4_project_files/js_sandbox/app.js
+++ V11411_Code File/S05/5_4_project_files/js_sandbox/app.js
@@ -5,36 +5,32 @@ function Person(firstName, lastName) {
 }
 
 // Greeting
-Person.prototype.greeting = function(){
+Person.prototype.greeting = function () {
   return `Hello there ${this.firstName} ${this.lastName}`;
 }
 
 const person1 = new Person('John', 'Doe');
 
-console.log(person1.greeting());
-
 // Customer constructor
 function Customer(firstName, lastName, phone, membership) {
   Person.call(this, firstName, lastName);
-
   this.phone = phone;
   this.membership = membership;
 }
 
-// Inherit the Person prototype methods
+// Inherit the Person prototype
 Customer.prototype = Object.create(Person.prototype);
 
 // Make customer.prototype return Customer()
 Customer.prototype.constructor = Customer;
 
-// Create customer
-const customer1 = new Customer('Tom', 'Smith', '555-555-5555', 'Standard');
-
+// Create a customer
+const customer1 = new Customer('Marco', 'Tedone', '123344', 'Standard');
 console.log(customer1);
 
 // Customer greeting
-Customer.prototype.greeting = function(){
-  return `Hello there ${this.firstName} ${this.lastName} welcome to our company`;
+Customer.prototype.greeting = function() {
+  return `Hello there ${this.firstName} ${this.lastName}. Welcome to our company.`;
 }
 
 console.log(customer1.greeting());
