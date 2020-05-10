0843a6c0a9ec1eccac9d5353148bcf99492c0858
Marco Tedone <marco.tedone@gmail.com>
2020-05-10 14:57:12 +0100

    Started the AJAX section

--- V11411_Code File/S07/7_3_project_files/js_sandbox/app.js
+++ V11411_Code File/S07/7_3_project_files/js_sandbox/app.js
@@ -1,51 +1,26 @@
 document.getElementById('button').addEventListener('click', loadData);
 
 function loadData() {
-  // Create an XHR Object
+  // Create an XHR object
   const xhr = new XMLHttpRequest();
 
-  // OPEN
+  // Open
   xhr.open('GET', 'data.txt', true);
 
-  // console.log('READYSTATE', xhr.readyState);
-
-  // Optional - Used for spinners/loaders
-  xhr.onprogress = function(){
-    console.log('READYSTATE', xhr.readyState);
+  xhr.onprogress = function() {
+    //Could put a spinner here.
   }
 
-  xhr.onload = function(){
-    console.log('READYSTATE', xhr.readyState);
-    if(this.status === 200) {
-      // console.log(this.responseText);
-      document.getElementById('output').innerHTML = `<h1>${this.responseText}</h1>`;
+  xhr.onload = function () {
+    if (this.status === 200) {
+      document.getElementById('output').innerHTML =
+          `<h1>${this.responseText}</h1>`;
     }
   }
 
-  // xhr.onreadystatechange = function() {
-  //   console.log('READYSTATE', xhr.readyState);
-  //   if(this.status === 200 && this.readyState === 4){
-  //     console.log(this.responseText);
-  //   }
-  // }
-
   xhr.onerror = function() {
-    console.log('Request error...');
+    console.log("Request error");
   }
 
   xhr.send();
-
-
-    // readyState Values
-    // 0: request not initialized 
-    // 1: server connection established
-    // 2: request received 
-    // 3: processing request 
-    // 4: request finished and response is ready
-
-
-  // HTTP Statuses
-  // 200: "OK"
-  // 403: "Forbidden"
-  // 404: "Not Found"
 }
