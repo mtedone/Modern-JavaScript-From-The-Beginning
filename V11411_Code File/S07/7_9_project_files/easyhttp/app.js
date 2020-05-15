b4923895d0267a5895bb564f0e2cfd78097fedd9
Marco Tedone <marco.tedone@gmail.com>
2020-05-15 21:18:21 +0100

    Worked with promises

--- V11411_Code File/S07/7_9_project_files/easyhttp/app.js
+++ V11411_Code File/S07/7_9_project_files/easyhttp/app.js
@@ -1,52 +1,56 @@
-const http = new easyHTTP;
+const posts = [
+  {title: 'Post one', body: 'This is post one'},
+  {title: 'Post two', body: 'This is post two'}
+];
 
-// Get Posts
-// http.get('https://jsonplaceholder.typicode.com/posts', function(err, posts) {
-//   if(err) {
-//     console.log(err);
-//   } else {
-//     console.log(posts);
-//   }
-// });
+// function createPost(post) {
+//   setTimeout(function() {
+//     posts.push(post);
+//   }, 2000);
+// }
+//
+// function getPosts() {
+//   setTimeout(function () {
+//     let output = '';
+//     posts.forEach(function (post) {
+//       output += `<li>${post.title}</li>`
+//     });
+//     document.body.innerHTML = output;
+//   }, 1000);
+// }
+//
+// createPost({title: 'Post three', body: 'This is post three'});
+// getPosts();
 
-// Get Single Post
-// http.get('https://jsonplaceholder.typicode.com/posts/1', function(err, post) {
-//   if(err) {
-//     console.log(err);
-//   } else {
-//     console.log(post);
-//   }
-// });
+function createPost(post) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function() {
+      posts.push(post);
 
-// Create Data
-const data = {
-  title: 'Custom Post',
-  body: 'This is a custom post'
-};
+      const error = false;
+      if (!error) {
+        resolve();
+      } else {
+        reject('Something went wrong');
+      }
+    }, 1000);
+  });
 
-// Create Post
-// http.post('https://jsonplaceholder.typicode.com/posts', data, function(err, post) {
-//   if(err) {
-//     console.log(err);
-//   } else {
-//     console.log(post);
-//   }
-// });
 
-// Update Post
-// http.put('https://jsonplaceholder.typicode.com/posts/5', data, function(err, post) {
-//   if(err) {
-//     console.log(err);
-//   } else {
-//     console.log(post);
-//   }
-// });
+}
 
-// Delete Post
-http.delete('https://jsonplaceholder.typicode.com/posts/1', function(err, response) {
-  if(err) {
-    console.log(err);
-  } else {
-    console.log(response);
-  }
-});
+function getPosts() {
+  setTimeout(function () {
+    let output = '';
+    posts.forEach(function (post) {
+      output += `<li>${post.title}</li>`
+    });
+    document.body.innerHTML = output;
+  }, 2000);
+}
+
+createPost({title: 'Post three', body: 'This is post three'})
+    .then(getPosts)
+    .catch(function (err) {
+      console.log(err);
+    });
