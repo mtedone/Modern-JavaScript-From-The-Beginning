2a1b05e7552e5ffe7d3ba4a640cb9c3a8bcfc152
Marco Tedone <marco.tedone@gmail.com>
2020-05-15 21:47:10 +0100

    Learned the Fetch API

--- V11411_Code File/S07/7_10_project_files/js_sandbox/app.js
+++ V11411_Code File/S07/7_10_project_files/js_sandbox/app.js
@@ -1,36 +1,54 @@
-const posts = [
-  {title: 'Post One', body:'This is post one'},
-  {title: 'Post Two', body: 'This is post two'}
-];
+document.getElementById('button1').addEventListener('click', getText);
+document.getElementById('button2').addEventListener('click', getJson);
+document.getElementById('button3').addEventListener('click', getApiData);
 
-function createPost(post) {
-  return new Promise(function(resolve, reject){
-    setTimeout(function() {
-      posts.push(post);
-
-      const error = false;
-
-      if(!error) {
-        resolve();
-      } else {
-        reject('Error: Something went wrong');
-      }
-    }, 2000);
-  });
+function getText() {
+  fetch('test.txt')
+      .then(function (response) {
+        return response.text();
+      })
+      .then(function (data) {
+        console.log(data);
+        document.getElementById('output')
+            .innerHTML = data;
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
 }
 
-function getPosts() {
-  setTimeout(function() {
-    let output = '';
-    posts.forEach(function(post){
-      output += `<li>${post.title}</li>`;
-    });
-    document.body.innerHTML = output;
-  }, 1000);
+function getJson() {
+  fetch('posts.json')
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        let output = '';
+        data.forEach(function (post) {
+          output += `<li>${post.title}</li>`;
+        });
+        document.getElementById('output')
+            .innerHTML = output;
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
 }
 
-createPost({title: 'Post Three', body: 'This is post three'})
-.then(getPosts)
-.catch(function(err) {
-  console.log(err);
-});
+function getApiData() {
+  fetch('https://api.github.com/users')
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        let output = '';
+        data.forEach(function (user) {
+          output += `<li>${user.login}</li>`;
+        });
+        document.getElementById('output')
+            .innerHTML = output;
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+}
