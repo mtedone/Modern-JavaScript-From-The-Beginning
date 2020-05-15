1fd0a9b86d5725625aaa8561a067b160b757e479
Marco Tedone <marco.tedone@gmail.com>
2020-05-15 21:01:38 +0100

    Created an ES5 easy http library

--- V11411_Code File/S07/7_8_project_files/easyhttp/app.js
+++ V11411_Code File/S07/7_8_project_files/easyhttp/app.js
@@ -1,10 +1,42 @@
-const http = new easyHTTP;
+const http = new easyHttp;
 
 // Get Posts
-http.get('https://jsonplaceholder.typicode.com/posts', function(err, posts) {
-  if(err) {
-    console.log(err);
+// http.get('https://jsonplaceholder.typicode.com/posts', function (error, posts) {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log(posts);
+//   }
+//
+// });
+
+// Create data
+const data = {
+  title: 'Custom Post',
+  body: 'This is a custom post'
+};
+
+// http.post('https://jsonplaceholder.typicode.com/posts', data, function (error, post) {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log(post);
+//   }
+// });
+
+// http.put('https://jsonplaceholder.typicode.com/posts/1', data, function (error, post) {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log(post);
+//   }
+// });
+
+http.delete('https://jsonplaceholder.typicode.com/posts/1', function (error, response) {
+  if (error) {
+    console.log(error);
   } else {
-    console.log(posts);
+    console.log(response);
   }
 });
+
