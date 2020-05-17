f36e208d9fa4e926974412377806db0ad3b433d5
Marco Tedone <marco.tedone@gmail.com>
2020-05-17 14:01:27 +0100

    Started learning regular expressions with metacharacters

--- V11411_Code File/S09/9_3_project_files/js_sandbox/app.js
+++ V11411_Code File/S09/9_3_project_files/js_sandbox/app.js
@@ -1,28 +1,27 @@
 let re;
-// Literal Characters
+// Literal characters
 re = /hello/;
 re = /hello/i;
 
-// Metacharacter Symbols
-re = /^h/i;           // Must start with
-re = / world$/i;     // Must ends with
-re = /^hello$/i;     // Must begin and end with
-re = /h.llo/i;      // Matches any ONE character
-re = /h*llo/i;      // Matches any character 0 or more times
-re = /gre?a?y/i;    // Optional character
-re = /gre?a?y\?/i;    // Escape character 
+// Meta character symbols
+re = /^h/i;           // Must start with h
+re = /d$/i;           // Must end with d
+re = /^hello world$/i // Must start and end
+re = /h.llo/i         // . accepts any character exactly once
+re = /h*llo/i         // * accepts any character zero or more times
+re = /gre?a?y/i       // e and a are optional
+re = /gre?a?y\?/i     // Escape characters for the ?
 
 
-// String to match
-const str = 'Gray?';
+// Str to match
+const str = 'grey?';
 
-
-// Log Results
+// Log results
 const result = re.exec(str);
 console.log(result);
 
 function reTest(re, str) {
-  if(re.test(str)) {
+  if (re.test(str)) {
     console.log(`${str} matches ${re.source}`);
   } else {
     console.log(`${str} does NOT match ${re.source}`);
