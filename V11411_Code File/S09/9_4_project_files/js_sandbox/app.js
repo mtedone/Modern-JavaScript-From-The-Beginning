2ac6bfa05e551b31d590ca33d531a19b4f2cd6e7
Marco Tedone <marco.tedone@gmail.com>
2020-05-17 19:59:20 +0100

    Started learning regular expressions with Character sets, quantifiers and grouping

--- V11411_Code File/S09/9_4_project_files/js_sandbox/app.js
+++ V11411_Code File/S09/9_4_project_files/js_sandbox/app.js
@@ -1,44 +1,44 @@
 let re;
-// Literal Characters
+// Literal characters
 re = /hello/;
 re = /hello/i;
 
-// Metacharacter Symbols
-re = /^h/i;           // Must start with
-re = / world$/i;     // Must ends with
-re = /^hello$/i;     // Must begin and end with
-re = /h.llo/i;      // Matches any ONE character
-re = /h*llo/i;      // Matches any character 0 or more times
-re = /gre?a?y/i;    // Optional character
-re = /gre?a?y\?/i;    // Escape character 
-
+// Meta character symbols
+re = /^h/i;           // Must start with h
+re = /d$/i;           // Must end with d
+re = /^hello world$/i // Must start and end
+re = /h.llo/i         // . accepts any character exactly once
+re = /h*llo/i         // * accepts any character zero or more times
+re = /gre?a?y/i       // e and a are optional
+re = /gre?a?y\?/i     // Escape characters for the ?
 
 // Brackets [] - Character Sets
-re = /gr[ae]y/i;      // Must be an a or e
-re = /[GF]ray/i;      // Must be a G or F
-re = /[^GF]ray/i;      // Match anything except a G or F
-re = /[A-Z]ray/;      // Match any uppercase letter
-re = /[a-z]ray/;      // Match any lowercase letter
-re = /[A-Za-z]ray/;   // Match any  letter
-re = /[0-9][0-9]ray/;      // Match any digit
+re = /gr[ae]y/i;      // Must be an a or an e
+re = /[GF]ray/i;      // Must be a G or an F
+re = /[^GF]ray/i;     // Must match anything except a G or an F
+re = /[A-Z]ray/;      // Matches any uppercase letter
+re = /[a-z]ray/;      // Matches any lowercase letter
+re = /[a-zA-Z]ray/;   // Matches any letter
+re = /[0-9]ray/;      // Matches any digit
 
 // Braces {} - Quantifiers
-re = /Hel{2}o/i;      // Must occur exactly {m} amount of times
-re = /Hel{2,4}o/i;      // Must occur exactly {m} amount of times
-re = /Hel{2,}o/i;      // Must occur at least {m} times
+re = /Hel{2}o/i;      // Matches exactly two 'l'
+re = /Hel{2,4}o/i;    // Matches two to four 'l'
+re = /Hel{2,}o/i;     // Matches at least two 'l'
 
-// Paretheses () - Grouping
-re = /^([0-9]x){3}$/
+// Parenthesis () - Grouping
+re = /([0-9]x){3}/;   // Matches any digit followed by an x, 3 times
 
-// String to match
-const str = '3x3x3x';
 
-// Log Results
+// Str to match
+const str = '9x8x7x';
+
+// Log results
 const result = re.exec(str);
 console.log(result);
 
 function reTest(re, str) {
-  if(re.test(str)) {
+  if (re.test(str)) {
     console.log(`${str} matches ${re.source}`);
   } else {
     console.log(`${str} does NOT match ${re.source}`);
