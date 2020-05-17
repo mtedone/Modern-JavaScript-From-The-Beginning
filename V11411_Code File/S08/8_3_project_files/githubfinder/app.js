259c02a4ef6e2da28036f1db77c414faea3fe851
Marco Tedone <marco.tedone@gmail.com>
2020-05-17 11:38:29 +0100

    Created the first card with GitHub information

--- V11411_Code File/S08/8_3_project_files/githubfinder/app.js
+++ V11411_Code File/S08/8_3_project_files/githubfinder/app.js
@@ -1,30 +1,26 @@
-// Init Github
 const github = new Github;
-// Init UI
 const ui = new UI;
-
-// Search input
 const searchUser = document.getElementById('searchUser');
 
-// Search input event listener
-searchUser.addEventListener('keyup', (e) => {
-  // Get input text
-  const userText = e.target.value;
+// Note: keyup looses the last character when typing too fast. Using input instead
+searchUser.addEventListener('input', (e) => {
 
-  if(userText !== ''){
-   // Make http call
-   github.getUser(userText)
-    .then(data => {
-      if(data.profile.message === 'Not Found') {
-        // Show alert
+    const userText = e.target.value;
 
-      } else {
-        // Show profile
-        ui.showProfile(data.profile);
-      }
-    })
-  } else {
-    // Clear profile
-    
-  }
-}); 
+    if (userText !== '') {
+        // Make http call
+        github.getUser(userText)
+            .then(data => {
+                if (data.profile.message === 'Not Found') {
+                    // Show alert
+
+                } else {
+                    // Show profile
+                    ui.showProfile(data.profile);
+                }
+            })
+        console.log(userText);
+    } else {
+        // Clear profile
+    }
+})
