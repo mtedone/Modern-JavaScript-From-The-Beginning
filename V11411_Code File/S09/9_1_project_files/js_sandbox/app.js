a74ef2f35f10b496178ddb503d8dc3177ef46174
Marco Tedone <marco.tedone@gmail.com>
2020-05-17 13:31:34 +0100

    Learnt error handling

--- V11411_Code File/S09/9_1_project_files/js_sandbox/app.js
+++ V11411_Code File/S09/9_1_project_files/js_sandbox/app.js
@@ -1,31 +1,25 @@
 const user = {email: 'jdoe@gmail.com'};
 
 try {
-  // Produce a ReferenceError
   // myFunction();
 
-  // Produce a TypeError
+  // Produce a type error
   // null.myFunction();
 
-  // Will produce SyntaxError
-  // eval('Hello World');
+  // Produce syntax error
+  // console.log(eval('Hello World'));
 
-  // Will produce a URIError
+  // Will produce a URI error
   // decodeURIComponent('%');
 
-  if(!user.name) {
-    //throw 'User has no name';
+  if (!user.name) {
+    // throw 'User has no name';
     throw new SyntaxError('User has no name');
   }
-
-} catch(e) {
-  console.log(`User Error: ${e.message}`);
-  // console.log(e);
-  // console.log(e.message);
+} catch (e) {
+  console.log(e);
   // console.log(e.name);
-  // console.log(e instanceof TypeError);
+  // console.log(e instanceof ReferenceError);
 } finally {
-  console.log('Finally runs reguardless of result...');
+  console.log('Finally runs');
 }
-
-console.log('Program continues...');
