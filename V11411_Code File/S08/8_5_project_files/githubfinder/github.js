ecb9991e63ce6a5ffbd6552bb076fae1fdfd7b2d
Marco Tedone <marco.tedone@gmail.com>
2020-05-17 13:08:49 +0100

    Added repos to the GitHub UI

--- V11411_Code File/S08/8_5_project_files/githubfinder/github.js
+++ V11411_Code File/S08/8_5_project_files/githubfinder/github.js
@@ -1,22 +1,38 @@
 class Github {
   constructor() {
-    this.client_id = 'd9308aacf8b204d361fd';
-    this.client_secret = '84969aeef73956f4ec9e8716d1840532802bb81b';
-    this.repos_count = 5;
+    // Not for production
+    this.client_id = '83fe06604b35ae6a251a';
+    this.client_secret = '956a41137ff03f53a1c351769ab3d5adc0b17b6a';
+    this.authenticationHeader = btoa(`${this.client_id}:${this.client_secret}`);
+    this.repos_count = 10;
     this.repos_sort = 'created: asc';
   }
 
   async getUser(user) {
-    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+    const userFetchUrl = `https://api.github.com/users/${user}`;
+    const reposFetchUrl = `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}`;
+    const profileResponse =
+        await fetch(`${userFetchUrl}`, {
+          method: 'GET',
+          headers: {
+            'Authorization': `Basic ${this.authenticationHeader}`
+          }
+        });
 
-    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+      const repoResponse =
+          await fetch(`${reposFetchUrl}`, {
+              method: 'GET',
+              headers: {
+                  'Authorization': `Basic ${this.authenticationHeader}`
+              }
+          });
 
     const profile = await profileResponse.json();
     const repos = await repoResponse.json();
 
     return {
-      profile,
-      repos
+        profile: profile,
+        repos: repos
     }
   }
 }
