0fa658fbb1f0eaf6b172346be03ab277f6c72d2b
Marco Tedone <marco.tedone@gmail.com>
2020-05-18 20:46:11 +0100

    Started with iterators and generators

--- V11411_Code File/S10/10_1_project_files/js_sandbox/app.js
+++ V11411_Code File/S10/10_1_project_files/js_sandbox/app.js
@@ -1,25 +1,23 @@
-// Iterator Example
+// // Create an iterator example
 // function nameIterator(names) {
 //   let nextIndex = 0;
-
+//
 //   return {
-//     next: function() {
+//     next: function () {
 //       return nextIndex < names.length ?
-//       { value: names[nextIndex++], done: false } :
-//       { done: true }
+//           { value : names[nextIndex++], done: false} :
+//           { done: true}
 //     }
 //   }
 // }
-
+//
 // // Create an array of names
-// const namesArr = ['Jack', 'Jill', 'John'];
-// // Init iterator and pass in the names array
-// const names = nameIterator(namesArr);
-
-// console.log(names.next().value);
-// console.log(names.next().value);
-// console.log(names.next().value);
-// console.log(names.next().value);
+// const namesArray = ['Jack', 'Jill', 'John'];
+//
+// // Initialise the iterator and pass the array
+// const names = nameIterator(namesArray);
+//
+// console.log(names.next())
 
 // Generator Example
 // function* sayNames() {
@@ -27,28 +25,17 @@
 //   yield 'Jill';
 //   yield 'John';
 // }
-
+//
 // const name = sayNames();
-
-// console.log(name.next().value);
-// console.log(name.next().value);
-// console.log(name.next().value);
-// console.log(name.next().value);
+// console.log(name.next());
 
 // ID Creator
 function* createIds() {
-  let index = 1;
-
+  let index = 0;
   while(true) {
     yield index++;
   }
 }
 
 const gen = createIds();
-
-console.log(gen.next().value);
-console.log(gen.next().value);
-console.log(gen.next().value);
-console.log(gen.next().value);
-console.log(gen.next().value);
 console.log(gen.next().value);
