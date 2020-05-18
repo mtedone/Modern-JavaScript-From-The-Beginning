d4a08f527210ba9d131a125aaebdf2eb386d8463
Marco Tedone <marco.tedone@gmail.com>
2020-05-18 18:13:57 +0100

    Completed Bootstrap form with validation and regular expressions

--- V11411_Code File/S09/9_6_project_files/userform/app.js
+++ V11411_Code File/S09/9_6_project_files/userform/app.js
@@ -1,4 +1,5 @@
-// Form Blur Event Listeners
+// Form blur event listeners
+
 document.getElementById('name').addEventListener('blur', validateName);
 document.getElementById('zip').addEventListener('blur', validateZip);
 document.getElementById('email').addEventListener('blur', validateEmail);
@@ -8,7 +9,7 @@ function validateName() {
   const name = document.getElementById('name');
   const re = /^[a-zA-Z]{2,10}$/;
 
-  if(!re.test(name.value)){
+  if (!re.test(name.value)) {
     name.classList.add('is-invalid');
   } else {
     name.classList.remove('is-invalid');
@@ -17,9 +18,9 @@ function validateName() {
 
 function validateZip() {
   const zip = document.getElementById('zip');
-  const re = /^[0-9]{5}(-[0-9]{4})?$/;
+  const re = /^[a-zA-Z]{2}[0-9]{1,2}\s\d{1,2}[a-zA-Z]{2}$/;
 
-  if(!re.test(zip.value)){
+  if (!re.test(zip.value)) {
     zip.classList.add('is-invalid');
   } else {
     zip.classList.remove('is-invalid');
@@ -28,9 +29,9 @@ function validateZip() {
 
 function validateEmail() {
   const email = document.getElementById('email');
-  const re = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+  const re = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]){2,5}$/;
 
-  if(!re.test(email.value)){
+  if (!re.test(email.value)) {
     email.classList.add('is-invalid');
   } else {
     email.classList.remove('is-invalid');
@@ -39,9 +40,9 @@ function validateEmail() {
 
 function validatePhone() {
   const phone = document.getElementById('phone');
-  const re = /^\(?\d{3}\)?[-. ]?\d{3}[-. ]?\d{4}$/;
+  const re = /^(\+)?(\d{2,3})?(\(\d\))?(\d{10})$/;
 
-  if(!re.test(phone.value)){
+  if (!re.test(phone.value)) {
     phone.classList.add('is-invalid');
   } else {
     phone.classList.remove('is-invalid');
