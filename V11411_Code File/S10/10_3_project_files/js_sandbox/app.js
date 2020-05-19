fcce3aed4f00a66254d16914bbb20482c40dccfb
Marco Tedone <marco.tedone@gmail.com>
2020-05-19 17:51:44 +0100

    Learnt about symbols

--- V11411_Code File/S10/10_3_project_files/js_sandbox/app.js
+++ V11411_Code File/S10/10_3_project_files/js_sandbox/app.js
@@ -1,31 +1,29 @@
-// Create a symbol
+// Create a Symbol
 // const sym1 = Symbol();
 // const sym2 = Symbol('sym2');
 
-// console.log(typeof sym2);
+// console.log(sym2);
 
-// console.log(Symbol('123') === Symbol('123'));
-// console.log(`Hello ${sym1.toString()}`);
+// No two symbols can be the same
+console.log(Symbol('123') === Symbol('123')); //Gives false
 
 // Unique Object Keys
 const KEY1 = Symbol();
 const KEY2 = Symbol('sym2');
 
 const myObj = {};
-
 myObj[KEY1] = 'Prop1';
 myObj[KEY2] = 'Prop2';
-myObj.key3 = 'Prop3';
-myObj.key4 = 'Prop4';
+myObj.key3 = 'Prop3'; // This is not a symbol but just a property - enumerable
+myObj.key4 = 'Prop4'; // This is not a symbol but just a property = enumerable
 
 // console.log(myObj[KEY1]);
-// console.log(myObj[KEY2]);
 
-// Symbols are not enumerable in for...in
+// Symbols are not enumerable in for..in but properties are
 // for(let i in myObj) {
-//   console.log(`${i}: ${myObj[i]}`);
+//     console.log(`${i}: ${myObj[i]}`);
 // }
 
 // Symbols are ignored by JSON.stringify
-console.log(JSON.stringify({key: 'prop'}));
-console.log(JSON.stringify({[Symbol('sym1')]: 'prop'}));
+console.log(JSON.stringify({key: 'Prop'}));
+console.log(JSON.stringify({[Symbol('sym1')]: 'Prop'}));
