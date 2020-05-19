05d7e5d76d2849f5ef3351a1761bb27a0f29abc8
Marco Tedone <marco.tedone@gmail.com>
2020-05-19 17:38:34 +0100

    Created a profile scroller

--- V11411_Code File/S10/10_2_project_files/profilescroller/app.js
+++ V11411_Code File/S10/10_2_project_files/profilescroller/app.js
@@ -33,25 +33,28 @@ nextProfile();
 // Next Event
 document.getElementById('next').addEventListener('click', nextProfile);
 
-// Next Profile Display
 function nextProfile() {
   const currentProfile = profiles.next().value;
-
-  if(currentProfile !== undefined) {
+  if (currentProfile !== undefined) {
     document.getElementById('profileDisplay').innerHTML = `
-      <ul class="list-group">
-        <li class="list-group-item">Name: ${currentProfile.name}</li>
-        <li class="list-group-item">Age: ${currentProfile.age}</li>
-        <li class="list-group-item">Location: ${currentProfile.location}</li>
-        <li class="list-group-item">Preference: ${currentProfile.gender} looking for ${currentProfile.lookingfor}</li>
-      </ul>
-    `;
+<ul class="list-group">
+    <li class="list-group-item">Name: ${currentProfile.name}</li>
+    <li class="list-group-item">Age: ${currentProfile.age}</li>
+    <li class="list-group-item">Location: ${currentProfile.location}</li>
+    <li class="list-group-item">Preference: ${currentProfile.gender} 
+        looking for ${currentProfile.lookingfor}</li>
+</li>
+</ul>  
+  `;
 
-    document.getElementById('imageDisplay').innerHTML = `<img src="${currentProfile.image}">`;
+    document.getElementById('imageDisplay').innerHTML = `
+<img src="${currentProfile.image}">  
+  `;
   } else {
     // No more profiles
     window.location.reload();
   }
+
 }
 
 // Profile Iterator
@@ -59,10 +62,10 @@ function profileIterator(profiles) {
   let nextIndex = 0;
 
   return {
-    next: function() {
-      return nextIndex < profiles.length ? 
-      { value: profiles[nextIndex++], done: false } : 
-      { done: true }
+    next: function () {
+      return nextIndex < profiles.length ?
+          { value: profiles[nextIndex++], done: false} :
+          { done: true}
     }
   };
 }
