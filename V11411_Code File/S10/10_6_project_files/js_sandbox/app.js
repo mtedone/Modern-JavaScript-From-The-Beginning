ab6192f0797ee2167e742494071728c30118d075
Marco Tedone <marco.tedone@gmail.com>
2020-05-19 18:34:07 +0100

    Learnt Sets

--- V11411_Code File/S10/10_6_project_files/js_sandbox/app.js
+++ V11411_Code File/S10/10_6_project_files/js_sandbox/app.js
@@ -1,44 +1,44 @@
-// SETS - Store unique values of any type
-
 const set1 = new Set();
 
 // Add values to set
 set1.add(100);
-set1.add('A string');
+set1.add('A String');
 set1.add({name: 'John'});
 set1.add(true);
-set1.add(100);
+set1.add(100); // Won't be added
 
 // const set2 = new Set([1, true, 'string']);
 // console.log(set2);
 
 // console.log(set1);
-
-// Get count
+//
+// // Get count
 // console.log(set1.size);
-
-// Check for values
+//
+// // Check for values
 // console.log(set1.has(100));
 // console.log(set1.has(50 + 50));
-// console.log(set1.has({name: 'John'}));
-
-// Delete from set
+// console.log(set1.has({name: 'John'})); // Will return false
+//
+// // Delete from the set
 // set1.delete(100);
-
 // console.log(set1);
 
-// ITERATING THROUGH SETS
+// Iterate through sets
 
-// For..of 
+// For..of
 // for(let item of set1) {
-//   console.log(item);
+//     console.log(item);
 // }
 
-// ForEach Loop
-// set1.forEach((value) => {
-//   console.log(value);
+// For each
+// set1.forEach(item => {
+//     console.log(item);
 // });
 
-// CONVERT SET TO ARRAY
-const setArr = Array.from(set1);
-console.log(setArr);
+// Converts sets into arrays
+const setArray = Array.from(set1);
+console.log(setArray);
+
+
+
