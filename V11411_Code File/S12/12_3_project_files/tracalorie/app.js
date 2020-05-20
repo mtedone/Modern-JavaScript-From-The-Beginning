646ca5d66ba3dcedc3b5534b0a1d161318733394
Marco Tedone <marco.tedone@gmail.com>
2020-05-20 18:32:36 +0100

    Tracalorie: Created modules skeleton

--- V11411_Code File/S12/12_3_project_files/tracalorie/app.js
+++ V11411_Code File/S12/12_3_project_files/tracalorie/app.js
@@ -1,10 +1,9 @@
 // Storage Controller
 
-
 // Item Controller
-const ItemCtrl = (function(){
+const ItemCtrl = (function () {
   // Item Constructor
-  const Item = function(id, name, calories){
+  const Item = function (id, name, calories) {
     this.id = id;
     this.name = name;
     this.calories = calories;
@@ -13,7 +12,7 @@ const ItemCtrl = (function(){
   // Data Structure / State
   const data = {
     items: [
-      {id: 0, name: 'Steak Dinner', calories: 1200},
+      {id: 0, name: 'Stake Dinner', calories: 1200},
       {id: 1, name: 'Cookie', calories: 400},
       {id: 2, name: 'Eggs', calories: 300}
     ],
@@ -23,36 +22,34 @@ const ItemCtrl = (function(){
 
   // Public methods
   return {
-    logData: function(){
+    logData: function () {
       return data;
     }
   }
+
 })();
-
-
 
 // UI Controller
-const UICtrl = (function(){
-  
+const UICtrl = (function () {
+
   // Public methods
   return {
 
   }
+
 })();
 
-
-
 // App Controller
-const App = (function(ItemCtrl, UICtrl){
+const App = (function (ItemCtrl, UICtrl) {
 
   // Public methods
   return {
-    init: function(){
-      console.log('Initializing App...');
+    init: function () {
+
     }
   }
-  
+
 })(ItemCtrl, UICtrl);
 
-// Initialize App
+// Initialise App
 App.init();
