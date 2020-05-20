8c7f9e408a9250ec47b6ce858a2326cf9c7b3e91
Marco Tedone <marco.tedone@gmail.com>
2020-05-20 17:26:55 +0100

    Learned the State Pattern

--- V11411_Code File/S11/11_7_project_files/js_sandbox/app.js
+++ V11411_Code File/S11/11_7_project_files/js_sandbox/app.js
@@ -1,64 +1,66 @@
-const PageState = function() {
-  let currentState = new homeState(this);
+const PageState = function () {
+  let currentState = new homeState;
 
-  this.init = function() {
+  this.init = function () {
     this.change(new homeState);
   }
 
-  this.change = function(state) {
+  this.change = function (state) {
     currentState = state;
   }
+
 };
 
-// Home State
-const homeState = function(page) {
-  document.querySelector('#heading').textContent = null;
-  document.querySelector('#content').innerHTML = `
-    <div class="jumbotron">
-    <h1 class="display-3">Hello, world!</h1>
-    <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-    <hr class="my-4">
-    <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-    <p class="lead">
-      <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
-    </p>
-  </div>
-  `;
+const homeState = function () {
+  document.getElementById('heading').textContent = null;
+  document.getElementById('content').innerHTML =
+      `
+<div class="jumbotron">
+  <h1 class="display-4">Hello, world!</h1>
+  <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+  <hr class="my-4">
+  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+  <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+</div>      
+      `;
+
 };
 
-// About State
-const aboutState = function(page) {
-  document.querySelector('#heading').textContent = 'About Us';
-  document.querySelector('#content').innerHTML = `
-    <p>This is the about page</p>
-`;
+// About state
+const aboutState = function () {
+  document.getElementById('heading').textContent = 'About Us';
+  document.getElementById('content').innerHTML =
+      `
+<p>This is the about page</p>
+      `;
 };
 
-// Contact State
-const contactState = function(page) {
-  document.querySelector('#heading').textContent = 'Contact Us';
-  document.querySelector('#content').innerHTML = `
-  <form>
+// Contact state
+const contactState = function () {
+  document.getElementById('heading').textContent = 'Contact Us';
+  document.getElementById('content').innerHTML =
+      `
+<form>
     <div class="form-group">
-      <label>Name</label>
-      <input type="text" class="form-control">
+        <label>Name</label>
+        <input type="text" class="form-control">
     </div>
     <div class="form-group">
-    <label>Email address</label>
-    <input type="email" class="form-control">
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-  </form>
-`;
+        <label>Email address</label>
+        <input type="email" class="form-control">
+    </div>
+    <button type="submit" class="btn btn-primary" value="Submit">Submit</button>
+</form>
+      `;
 };
 
-// Instantiate pageState
+// Instantiate page state
 const page = new PageState();
 
 // Init the first state
 page.init();
 
-// UI Vars
+// UI variables
 const home = document.getElementById('home'),
       about = document.getElementById('about'),
       contact = document.getElementById('contact');
@@ -66,20 +68,17 @@ const home = document.getElementById('home'),
 // Home
 home.addEventListener('click', (e) => {
   page.change(new homeState);
-
   e.preventDefault();
 });
 
-// About
+// About us
 about.addEventListener('click', (e) => {
   page.change(new aboutState);
-
   e.preventDefault();
 });
 
-// Contact
+// Contact Us
 contact.addEventListener('click', (e) => {
   page.change(new contactState);
-
   e.preventDefault();
 });
